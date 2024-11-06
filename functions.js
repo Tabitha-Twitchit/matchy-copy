cc80f524d5e1069c30b71d556f965efc4496dbdc
Tabitha-Twitchit <tabitha@tabithanikolai.com>
2024-11-06 14:21:38 -0600

    at 7.6 part 1

--- functions.js
+++ functions.js
@@ -20,9 +20,21 @@
 var friends = [];
 
 function getRandom(animals){
-    let indexedAnimal = Math.random() * animals.length;
-    return 
+    // initialize a variable, assigning it a random number
+    // specifically using the "floor" method makes it an int rather than a float
+    // let indexedAnimal = Math.floor(Math.random() * animals.length);
+    // console.log(indexedAnimal);
+    // return indexedAnimal;
+    // I'm hazy here. The instructions specifically say to return an index, but 
+    // this could be done without declaring the variable, and referencing it,
+    // possibly through adding in a return function. Like so:
+    return Math.floor(Math.random() * animals.length);
 }
+// console.log(indexedAnimal);
+
+// friends.push(animals[indexedAnimal]["name"]);
+// console.log("my friends array: " + friends);
+
 
 
 //////////////////////////////////////////////////////////////////////
