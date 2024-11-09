bc57692ac7c96bb2cb58479270f2838dcc61ba8d
Tabitha-Twitchit <tabitha@tabithanikolai.com>
2024-11-09 17:28:16 -0600

    Completed Matchy

--- functions.js
+++ functions.js
@@ -10,16 +10,16 @@
  * 
  *
  */
+// const { animals } = require("./data");
 
 //////////////////////////////////////////////////////////////////////
 // Step 1 - Search ///////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
-console.log(animals);
 
 function search(arr, animalToSearch){
     console.log(animalToSearch);
     console.log(arr);
-
+    
     for (let i = 0; i < arr.length; i++ ){
         if(arr[i].name === animalToSearch){
             // console.log(animals[i]);
@@ -34,18 +34,65 @@ function search(arr, animalToSearch){
 // Step 2 - Replace //////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
-
+function replace(animals, name, replacement){
+    console.log(animals);
+    console.log(replacement);
+    console.log(name);
+    
+    for (let i = 0; i < animals.length; i++){
+        if(animals[i].name === name){
+            // animals.splice([i], 1, replacement);
+            animals[i] = replacement;
+       }
+    }
+}
 
 //////////////////////////////////////////////////////////////////////
 // Step 3 - Remove ///////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
+function remove(animals, name){
+
+    for (let i = 0 ; i < animals.length ; i++){
+        if(animals[i].name === name){
+            animals.splice([i], 1);
+        }
+    }
+}
 
 
 //////////////////////////////////////////////////////////////////////
 // Step 4 - Add ///////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////
 
+function add(animals, animal){
+    // establish a bool outside the loop to check for later
+    // (tried executing logic in the loop, but ofc this repeats for the loop
+    // length causing weird outcomes that camouflage in the test conditions
+    // you can ofc remember to check the test conditions to see if the number
+    //  of errors align with the text cond vs the loop iterations.)
+    let isUnique = true;
+
+    // if animal has a name of length greater than 0 && species name same
+    if(animal.name.length > 0 && animal.species.length > 0){
+        // then loop through the animals array and check that no one else has that name
+        for(let i = 0; i < animals.length ; i++ ){
+            // if they match...
+            if(animal.name ===  animals[i].name){
+                // assign our isUnique bool to false.
+                isUnique = false;
+                // use a BREAK statement to stop looping.
+                break;     
+            }
+        }
+        // Then OUTSIDE the for loop but INSIDE the larger IF statement
+        // Check that is unique is still true. And if so...
+        if(isUnique === true){
+            // push the verified unique animal argument onto the animal array 
+            animals.push(animal);
+        }
+    }
+}
 
 
 /**
